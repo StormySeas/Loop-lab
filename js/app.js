85472c0f796bd800d6f0949996f1a609518b7aef
Julian <julianboyd0@gmail.com>
2018-09-11 15:06:46 -0500

    this is my first commit to git

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//Javascript is dumb...
+// console.log("Making your way in the world today takes everything you've got.");
+// console.log("Taking a break from all your worries, sure would help a lot.");
+// console.log("Wouldn't you like to get away?");
+// console.log("Sometimes you want to go");
+// console.log("Where everybody knows your name,");
+// console.log("and they're always glad you came.");
+// console.log("You wanna be where you can see,");
+// console.log("our troubles are all the same");
+// console.log("You wanna be where" + "everybody knows");
+// console.log("Your name.");
+
+// let num = 1;
+// while (num <= 500) {
+//   console.log('Ginger chocolate honey patties ' + num);
+//   num++
+// }
+
+// let num = 1;
+// while (num <= 1000) {
+//   console.log('Current Loop number is ' + '=> ' + num);
+//   num++
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+for (let i = 0; i <= 635; i++){
+  if = (i == 100) {
+  break;
+
+  console.log(i);
+}
+}
